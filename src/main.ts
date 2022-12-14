edf3c4bce3e739ec8d86112f51e61ccd6b3d13eb
Patryk Szabat <szabat.patryk@gmail.com>
2022-12-14 11:21:46 +0100

    ts

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
-export const test = []
+export const left = document.querySelector('.left') as HTMLDivElement
+export const right = document.querySelector('.right') as HTMLDivElement
+export const container = document.querySelector('.container') as HTMLElement
+
+left.addEventListener('mouseenter', () => container.classList.add('hover-left'))
+left.addEventListener('mouseleave', () => container.classList.remove('hover-left'))
+
+right.addEventListener('mouseenter', () => container.classList.add('hover-right'))
+right.addEventListener('mouseleave', () => container.classList.remove('hover-right'))
